62b0fb4bb2525b56e15b41cdac28ec05ee2f2e9c
tongtn <versnoontt02@gmail.com>
2016-09-29 10:30:48 +0800

    修改主页面中入库单信息为库存管理 | 去掉装备信息初始化数据

--- src/main/java/io/jianxun/business/web/WeaponController.java
+++ src/main/java/io/jianxun/business/web/WeaponController.java
@@ -68,19 +68,4 @@ public class WeaponController extends BusinessController<Weapon> {
 		model.addAttribute("categories", dataDicService.getSelectOptions(dics));
 	}
 
-	@Override
-	protected void otherPageDate(Model model) {
-		super.otherPageDate(model);
-		// 测试数据
-		if (getEntityService().findAll().isEmpty()) {
-			for (int i = 0; i < 40; i++) {
-				Weapon entity = new Weapon();
-				entity.setBarcode("1111" + "-" + i);
-				entity.setCode("999" + "-" + i);
-				entity.setName("测试装备" + "-" + i);
-				getEntityService().save(entity);
-			}
-		}
-	}
-
 }
