f06328aeebd29bd2b9d849f40b34f483d7824f79
tongtn <versnoontt02@gmail.com>
2016-10-24 17:21:34 +0800

    增加简单的操作日志功能

--- src/main/java/io/jianxun/common/config/WebSecurity.java
+++ src/main/java/io/jianxun/common/config/WebSecurity.java
@@ -3,12 +3,17 @@ package io.jianxun.common.config;
 import java.util.Collection;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 
 import io.jianxun.business.domain.User;
 
 public class WebSecurity {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	/**
 	 * 
 	 * 通过url进行权限判断的统一入口
@@ -22,6 +27,7 @@ public class WebSecurity {
 	 */
 	public boolean check(Authentication authentication, String domain, String operate) {
 		if (authentication == null || !authentication.isAuthenticated()) {
+			logger.error("登录验证信息为空");
 			return false;
 		}
 		if (StringUtils.isEmpty(domain))
@@ -29,27 +35,38 @@ public class WebSecurity {
 		if (StringUtils.isEmpty(operate))
 			return false;
 		// 路径中business/ajax统一为只要登录就能使用
-		if ("ajax".equals(operate))
+		if ("ajax".equals(operate)) {
+			logger.debug("执行ajax操作");
 			return true;
+		}
+		String perStr = getPermissionStr(domain, operate);
+		
 		try {
-			User user = ((User) authentication.getPrincipal());
 			// 管理员拥有所有权限
-			if (user.getId() != null && 1L == user.getId())
+			User user = ((User) authentication.getPrincipal());
+			if (user.getId() != null && 1L == user.getId()) {
+				logger.debug("登录用户管理员,执行操作" + perStr);
 				return true;
+			}
 		} catch (Exception e) {
 			return false;
 		}
-		String perStr = getPermissionStr(domain, operate);
+		logger.debug("用户:" + ((User) authentication.getPrincipal()).getUsername() + "执行操作：" + perStr);
 		Collection<? extends GrantedAuthority> as = authentication.getAuthorities();
 		for (GrantedAuthority grantedAuthority : as) {
 			// 判断列表权限 只要有任何一个该模型的操作权限 列表权限都返回true
 			if (isPagePerStr(operate)) {
-				if (grantedAuthority.getAuthority().startsWith(domain.toUpperCase()))
+				if (grantedAuthority.getAuthority().startsWith(domain.toUpperCase())) {
+					logger.debug("权限验证通过");
 					return true;
+				}
 			}
-			if (grantedAuthority.getAuthority().equals(perStr))
+			if (grantedAuthority.getAuthority().equals(perStr)) {
+				logger.debug("权限验证通过");
 				return true;
+			}
 		}
+		logger.debug("权限验证失败");
 		return false;
 	}
 
